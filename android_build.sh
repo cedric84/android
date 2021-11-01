d52fd59fab7aab635a0b48a2d627d82e6def3882
cedric84 <44369985+cedric84@users.noreply.github.com>
2021-11-01 15:19:07 +0100

    Update android_build.sh

--- android_build.sh
+++ android_build.sh
@@ -60,7 +60,7 @@ echo ________
 ${ANDROID_ADB} wait-for-device
 ${ANDROID_ADB} devices
 #${ANDROID_ADB} shell "while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;"
-while [[ -z ${ANDROID_ADB} shell getprop sys.boot_completed ]]
+while [[ -z $(${ANDROID_ADB} shell getprop sys.boot_completed) ]]
 do
 	sleep 1
 done
