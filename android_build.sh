bdcd92d19ce87b23bd776e2d0967e35fe4be93a4
cedric84 <44369985+cedric84@users.noreply.github.com>
2021-11-01 09:02:19 +0100

    Update android_build.sh

--- android_build.sh
+++ android_build.sh
@@ -17,11 +17,11 @@ CC=$(find ${ANDROID_NDK_LATEST_HOME} -name ${MY_PREFIX}clang)
 ${CC} -Wall -Werror -o./app	\
 	./main.c
 
-echo ________
-${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager "system-images;android-16;default;armeabi-v7a"
-echo ________
-echo -ne '\n' | \
-${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager -s create avd -n "armv7a-api16" -k "system-images;android-16;default;armeabi-v7a"
+#echo ________
+#${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager "system-images;android-16;default;armeabi-v7a"
+#echo ________
+#echo -ne '\n' | \
+#${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager -s create avd -n "armv7a-api16" -k "system-images;android-16;default;armeabi-v7a"
 #echo ________
 #${ANDROID_HOME}/emulator/emulator -list-avds
 #${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager list target -c
