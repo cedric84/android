29bab44922ba4527397fd2635067bc8ce9d976b8
cedric <dabrao@gmail.com>
2021-11-01 05:57:42 +0100

    adding sources.

---
+++ android_build.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+#---Definitions---#
+#ANDROID_HOME=/home/cedric/apps/x86_64-gnu/dev/android-sdk
+#ANDROID_EMULATOR_SN=emulator-5554
+#ANDROID_ADB="${ANDROID_HOME}/platform-tools/adb -s ${ANDROID_EMULATOR_SN}"
+#ANDROID_DEST_PATH=/data/local
+#ANDROID_API=16
+#ANDROID_API=24
+
+#---Restart as root---#
+#${ANDROID_ADB} root
+
+#---Build---#
+CC=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi${ANDROID_API}-clang
+${CC} -Wall -Werror -o./app	\
+	./main.c
+
+#---Copy files---#
+#${ANDROID_ADB} push ./app ${ANDROID_DEST_PATH}
+
+#---Run---#
+#${ANDROID_ADB} shell date
+#${ANDROID_ADB} shell ${ANDROID_DEST_PATH}/app
+#${ANDROID_ADB} shell date
+
+#---Remove the application---#
+#${ANDROID_ADB} shell rm ${ANDROID_DEST_PATH}/app
+
+#---Restart as unroot---#
+#${ANDROID_ADB} unroot
---
+++ main.c
@@ -0,0 +1,21 @@
+/**
+ * @brief		The application entry point.
+ * @file
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * @brief		The application entry point.
+ * @param		[in]	argc	The number of arguments.
+ * @param		[in]	argv	The arguments values.
+ * @return		Returns EXIT_SUCCESS on success.
+ */
+extern int
+main(int argc, char* argv[])
+{
+	printf("%s started\n", __func__);
+	printf("%s terminated\n", __func__);
+	return EXIT_SUCCESS;
+}
