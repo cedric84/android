762fbb6bcd35d2dc1a8eaa9460aa69a03597d305
cedric84 <44369985+cedric84@users.noreply.github.com>
2021-11-01 10:03:42 +0100

    Update android_build.sh

--- android_build.sh
+++ android_build.sh
@@ -37,12 +37,12 @@ ANDROID_EMULATOR=${ANDROID_HOME}/emulator/emulator
 echo ________
 ${ANDROID_AVDMANAGER} list avd -c
 echo ________
-${ANDROID_ADB} kill-server
+#${ANDROID_ADB} kill-server
 #echo -ne 'whoami\npwd\nexit\n' | ${ANDROID_EMULATOR} -avd armv7a-api16 -no-window -shell
 ${ANDROID_EMULATOR} -avd armv7a-api16 -no-window &
 echo ________
 sleep 120s
-${ANDROID_ADB} start-server
+#${ANDROID_ADB} start-server
 ${ANDROID_ADB} devices
 #echo ________
 #ps
