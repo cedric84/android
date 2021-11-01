634059e22a0c3dafa58ddd31ebf131909d5e09d6
cedric84 <44369985+cedric84@users.noreply.github.com>
2021-11-01 15:27:32 +0100

    Update android_build.sh

--- android_build.sh
+++ android_build.sh
@@ -60,10 +60,7 @@ echo ________
 ${ANDROID_ADB} wait-for-device
 ${ANDROID_ADB} devices
 #${ANDROID_ADB} shell "while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;"
-while [[ -z $(${ANDROID_ADB} shell getprop sys.boot_completed) ]]
-do
-	sleep 1
-done
+${ANDROID_ADB} shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 ${ANDROID_ADB} shell ls -la /sdcard
 echo ________
 ${ANDROID_ADB} shell ls -la /storage
