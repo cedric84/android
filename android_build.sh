20cfded80756658ecc271051e839abc3fa3b19cd
cedric <dabrao@gmail.com>
2021-11-02 04:28:55 +0100

    updating building script.

--- android_build.sh
+++ android_build.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
 #---Definitions---#
-#ANDROID_HOME=/home/cedric/apps/x86_64-gnu/dev/android-sdk
-#ANDROID_EMULATOR_SN=emulator-5554
-#ANDROID_ADB="${ANDROID_HOME}/platform-tools/adb -s ${ANDROID_EMULATOR_SN}"
-ANDROID_DEST_PATH=/data/local
-ANDROID_API=16
-#ANDROID_API=24
-MY_PREFIX=armv7a-linux-androideabi${ANDROID_API}-
+MY_ANDROID_AVDMANAGER=${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager
+MY_ANDROID_ADB=${ANDROID_HOME}/platform-tools/adb
+MY_ANDROID_EMULATOR=${ANDROID_HOME}/emulator/emulator
+MY_ANDROID_MKSDCARD=${ANDROID_HOME}/emulator/mksdcard
+MY_ANDROID_AVD_NAME=armv7a-api16
+MY_ANDROID_EMULATOR_PORT=5554
+MY_ANDROID_EMULATOR_SN=emulator-${MY_ANDROID_EMULATOR_PORT}
+MY_ANDROID_DEST_PATH=/data/local
+MY_ANDROID_API=16
+#MY_ANDROID_API=24
+MY_PREFIX=armv7a-linux-androideabi${MY_ANDROID_API}-
 
 #---Build---#
 CC=$(find ${ANDROID_NDK_LATEST_HOME} -name ${MY_PREFIX}clang)
@@ -15,63 +19,47 @@ ${CC} -Wall -Werror -o./app	\
 	./main.c
 
 #echo ________
-#${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager "system-images;android-16;default;armeabi-v7a"
-#echo ________
-#echo -ne '\n' | \
-#${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager -s create avd -n "armv7a-api16" -k "system-images;android-16;default;armeabi-v7a"
-#echo ________
 #${ANDROID_HOME}/emulator/emulator -list-avds
 #${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager list target -c
 #echo ________
 #${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager list device -c
-#echo ________
-#${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager --list
-#echo ________
+echo ________
+${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager --list --include_obsolete
+echo ________
 
-ANDROID_AVDMANAGER=${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager
-ANDROID_ADB=${ANDROID_HOME}/platform-tools/adb
-ANDROID_EMULATOR=${ANDROID_HOME}/emulator/emulator
-ANDROID_MKSDCARD=${ANDROID_HOME}/emulator/mksdcard
-ANDROID_AVD_NAME=armv7a-api16
-echo ________
-${ANDROID_AVDMANAGER} list avd -c
-echo ________
-#${ANDROID_MKSDCARD} 128M ./mySdCard.img
+#echo ________
+#${MY_ANDROID_AVDMANAGER} list avd -c
+#echo ________
+#${MY_ANDROID_MKSDCARD} 128M ./mySdCard.img
 #mkdir ./sdcard
-#sudo mount -o loop,sync,uid=$(echo `whoami`) ~/.android/avd/${ANDROID_AVD_NAME}.avd/sdcard.img ./sdcard
+#sudo mount -o loop,sync,uid=$(echo `whoami`) ~/.android/avd/${MY_ANDROID_AVD_NAME}.avd/sdcard.img ./sdcard
 #sudo mount -o loop,sync,uid=$(echo `whoami`) ./mySdCard.img ./sdcard
 #mv ./app ./sdcard
 #sudo umount ./sdcard
 #rmdir ./sdcard
-echo ________
-${ANDROID_EMULATOR} -avd ${ANDROID_AVD_NAME} -no-window -no-audio -no-snapshot -sdcard ./mySdCard.img &
-echo ________
-${ANDROID_ADB} wait-for-device
-${ANDROID_ADB} devices
-${ANDROID_ADB} shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
-#${ANDROID_ADB} shell 'while [[ -z $(ls /sdcard/) ]]; do sleep 1; done;'
-#${ANDROID_ADB} shell ls -la /sdcard/
 #echo ________
-#${ANDROID_ADB} shell ls -la /storage/
-#echo ________
-#${ANDROID_ADB} shell ls -la /mnt/sdcard/
-#echo ________
-#${ANDROID_ADB} shell /sdcard/app
-#echo ________
-#${ANDROID_ADB} shell /mnt/sdcard/app
-#echo ________
+
+#---Start the emulator---#
+#${MY_ANDROID_EMULATOR} -avd ${MY_ANDROID_AVD_NAME} -no-window -no-audio -no-snapshot -sdcard ./mySdCard.img &
+${MY_ANDROID_EMULATOR} -avd ${MY_ANDROID_AVD_NAME} -port ${MY_ANDROID_EMULATOR_PORT} -no-window -no-audio -no-snapshot &
+
+#---Wait for the emulator to be ready---#
+${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} wait-for-device
+#${MY_ANDROID_ADB} devices
+${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
+#${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} shell 'while [[ -z $(ls /sdcard/) ]]; do sleep 1; done;'
 
 #---Restart as root---#
-${ANDROID_ADB} root
+${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} root
 
 #---Copy files---#
-${ANDROID_ADB} push ./app ${ANDROID_DEST_PATH}
+${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} push ./app ${MY_ANDROID_DEST_PATH}
 
 #---Run---#
-${ANDROID_ADB} shell ${ANDROID_DEST_PATH}/app
+${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} shell ${MY_ANDROID_DEST_PATH}/app
 
 #---Remove the application---#
-#${ANDROID_ADB} shell rm ${ANDROID_DEST_PATH}/app
+#${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} shell rm ${MY_ANDROID_DEST_PATH}/app
 
 #---Restart as unroot---#
-#${ANDROID_ADB} unroot
+#${MY_ANDROID_ADB} -s ${MY_ANDROID_EMULATOR_SN} unroot
